c0bd7b7e3f450df6e939eac36fce062ebb2a8fc3
Esraq <esraq26552@gmail.com>
2023-07-26 22:31:59 +0600

    Hello World

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\HelloController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+///Route::get('/','App/Http/Controllers/HelloController@hello');
+
+Route::get('/', [HelloController::class, 'hello']);
